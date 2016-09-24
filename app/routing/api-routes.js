8e6e22a57462eb967c22085da954b4d41b3cda07
Spencer Charest <spencercharest@Spencer-MacBook-Pro.local>
2016-09-24 16:16:22 -0700

    added mysql

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,4 +1,18 @@
+//MY sql password in a separate key file
+var key = require('../../keys.js');
+
+//node modules
 var path = require('path');
+var mysql = require('mysql');
+
+//sql connection
+var connection = mysql.createConnection({
+    host: "mydbinstance.coacymxlgea4.us-west-2.rds.amazonaws.com",
+    port: 3306,
+    user: "spencercharest",
+    password: key,
+    database: "Bamazon"
+});
 
 module.exports = function(app){
 
