3c989bfe6832bc99916a381f784f5150323702a2
Spencer Charest <spencercharest@Spencer-MacBook-Pro.local>
2016-09-27 12:08:30 -0700

    did some styling and user validation

--- app/public/javascript/script.js
+++ app/public/javascript/script.js
@@ -1,4 +1,55 @@
+var nameComplete = false;
+var photoComplete = false;
+
+function checkForm(){
+  if(nameComplete && photoComplete){
+    $("#submit-button").removeAttr("disabled");
+    $('#instructions').hide();
+  } else {
+    $("#submit-button").attr("disabled",true);
+  }
+}
+  function hasSuccess(divID,spanID){
+    $(divID).removeClass("has-error has-feedback");
+    $(divID).addClass("has-success has-feedback");
+    $(spanID).removeClass("glyphicon glyphicon-remove form-control-feedback");
+    $(spanID).addClass("glyphicon glyphicon-ok form-control-feedback");
+  }
+  function hasError(divID,spanID){
+    $(divID).removeClass("has-success has-feedback");
+    $(divID).addClass("has-error has-feedback");
+    $(spanID).removeClass("glyphicon glyphicon-ok form-control-feedback");
+    $(spanID).addClass("glyphicon glyphicon-remove form-control-feedback");
+  }
+
+
 $(document).ready(function(){
+
+  $("#name").keyup(function(){
+    if($("name").val() != ""){
+      nameComplete = true;
+      hasSuccess("#name-group","#name-span");
+    }
+    else{
+      nameComplete = false;
+      hasError("#name-group","#name-span");
+    }
+    checkForm();
+  });
+
+  $("#photo").keyup(function(){
+    if($("photo").val() != ""){
+      console.log($('#photo').val());
+      photoComplete = true;
+      hasSuccess("#photo-group","#photo-span");
+    }
+    else{
+      photoComplete = false;
+      hasError("#photo-group","#photo-span");
+    }
+    checkForm();
+  });
+
   $('#submit-button').on('click', function(){
     var newFriend = {
       name: $('#name').val().trim(),
@@ -16,7 +67,7 @@ $(document).ready(function(){
     }
     $.post('http://localhost:3000/api/friends', newFriend).then(function(response){
       //change link to Heroku link
-      console.log(response);
+
       //modal pop up
       $('#match-image').attr('src', response.photo);
       $('#match-name').text(response.name);
